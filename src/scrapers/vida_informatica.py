9de807648e5d4dccc779542a1b3e5e48341be35b
johanseb15 <johanseb15@hotmail.com>
2026-07-28 13:16:13 -0300

    Inyecta downloader en scraper Vida Informatica

--- src/scrapers/vida_informatica.py
+++ src/scrapers/vida_informatica.py
@@ -3,9 +3,17 @@ from src.extractor import extraer_datos
 from src.scrapers.base import BaseScraper
 
 
+class _DownloaderPorDefecto:
+    def descargar(self, url: str) -> str:
+        return descargar_html(url)
+
+
 class VidaInformaticaScraper(BaseScraper):
     URL = "https://vidainformatica.com.ar/listado-de-precios-zona-1/"
 
+    def __init__(self, downloader=None):
+        self.downloader = downloader or _DownloaderPorDefecto()
+
     def obtener_servicios(self):
-        html = descargar_html(self.URL)
+        html = self.downloader.descargar(self.URL)
         return extraer_datos(html)
