cec1916ad9276b39e36a51c8252a201318072e91
johanseb15 <johanseb15@hotmail.com>
2026-07-28 14:52:12 -0300

    Expone empresas en consulta API

--- src/api/main.py
+++ src/api/main.py
@@ -1,13 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
-app = FastAPI()
+from src.repositorio import RepositorioSQLite
+from src.reporte import generar_resumen_servicio
+
+
+app = FastAPI(
+    title="Enki API",
+    version="0.1.0"
+)
+
+
+def obtener_repositorio():
+    return RepositorioSQLite("enki.db")
 
 
 @app.get("/servicios/{nombre_servicio}")
-def consultar_servicio(nombre_servicio: str):
-    return {
-        "servicio": nombre_servicio,
-        "precio_minimo": 29816,
-        "precio_promedio": 33862,
-        "precio_maximo": 46000,
-    }
+def consultar_servicio(
+    nombre_servicio: str,
+    repo: RepositorioSQLite = Depends(obtener_repositorio)
+):
+    servicios = repo.obtener_todos()
+
+    resumen = generar_resumen_servicio(
+        servicios,
+        nombre_servicio
+    )
+
+    servicios_filtrados = [
+        servicio
+        for servicio in servicios
+        if servicio.servicio.lower() == nombre_servicio.lower()
+    ]
+
+    resumen["empresas"] = [
+        {
+            "empresa": servicio.empresa,
+            "precio": servicio.precio_local
+        }
+        for servicio in servicios_filtrados
+    ]
+
+    return resumen
